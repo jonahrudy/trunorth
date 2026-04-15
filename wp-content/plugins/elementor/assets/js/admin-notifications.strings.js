7c39cb7b3249c54295232cad752003770ff50e4f
jonahrudy <110498996+jonahrudy@users.noreply.github.com>
2026-04-15 16:48:28 -0700

    Updated/Added simply-static-1-1776295980

--- wp-content/plugins/elementor/assets/js/admin-notifications.strings.js
+++ wp-content/plugins/elementor/assets/js/admin-notifications.strings.js
@@ -1,3 +1,1 @@
-__( 'What\'s New', 'elementor' );
-__( 'What\'s New', 'elementor' );
-__( "What's New", 'elementor' );
+__('What\'s New','elementor');__('What\'s New','elementor');__("What's New",'elementor');
