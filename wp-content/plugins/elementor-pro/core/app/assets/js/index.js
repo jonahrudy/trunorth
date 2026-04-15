f78f95e9c661e6493af3c1156db7f5503c729a79
jonahrudy <110498996+jonahrudy@users.noreply.github.com>
2026-04-15 16:54:34 -0700

    Updated/Added simply-static-1-1776295980

--- wp-content/plugins/elementor-pro/core/app/assets/js/index.js
+++ wp-content/plugins/elementor-pro/core/app/assets/js/index.js
@@ -1,5 +1,1 @@
-import Module from '../../modules/site-editor/assets/js/site-editor';
-import ImportExportCustomizationModule from '../../modules/import-export-customization/assets/js/module';
-
-new Module();
-new ImportExportCustomizationModule();
+import Module from'../../modules/site-editor/assets/js/site-editor';import ImportExportCustomizationModule from'../../modules/import-export-customization/assets/js/module';new Module();new ImportExportCustomizationModule();
