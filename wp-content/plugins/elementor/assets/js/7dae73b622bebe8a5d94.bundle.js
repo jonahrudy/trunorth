4748c0784004b6a8267c88ab9bad5b812e741b1c
jonahrudy <110498996+jonahrudy@users.noreply.github.com>
2026-04-15 16:49:29 -0700

    Updated/Added simply-static-1-1776295980

--- wp-content/plugins/elementor/assets/js/7dae73b622bebe8a5d94.bundle.js
+++ wp-content/plugins/elementor/assets/js/7dae73b622bebe8a5d94.bundle.js
@@ -1,14 +1,5 @@
-"use strict";
-(self["webpackChunkelementor"] = self["webpackChunkelementor"] || []).push([["node_modules_elementor_elementor-one-assets_locales_pl-PL_common_json"],{
-
-/***/ "../node_modules/@elementor/elementor-one-assets/locales/pl-PL/common.json":
+"use strict";(self["webpackChunkelementor"]=self["webpackChunkelementor"]||[]).push([["node_modules_elementor_elementor-one-assets_locales_pl-PL_common_json"],{"../node_modules/@elementor/elementor-one-assets/locales/pl-PL/common.json":
 /*!*********************************************************************************!*\
   !*** ../node_modules/@elementor/elementor-one-assets/locales/pl-PL/common.json ***!
   \*********************************************************************************/
-/***/ ((module) => {
-
-module.exports = /*#__PURE__*/JSON.parse('{"example":{"component":"Przykładowy komponent"}}');
-
-/***/ })
-
-}]);
+((module)=>{module.exports=JSON.parse('{"example":{"component":"Przykładowy komponent"}}');})}]);
--- wp-content/plugins/elementor/assets/js/editor-loader-v1.js
+++ wp-content/plugins/elementor/assets/js/editor-loader-v1.js
@@ -1,11 +1,5 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
+(()=>{"use strict";
 /*!*******************************************************!*\
   !*** ../core/editor/loader/v1/js/editor-loader-v1.js ***!
   \*******************************************************/
-
-
-window.elementor.start();
-/******/ })()
-;
-//# sourceMappingURL=editor-loader-v1.js.map
+window.elementor.start();})();
