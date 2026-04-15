86da5ca56c712afb92da35b01c11a1e7c1da4acb
jonahrudy <110498996+jonahrudy@users.noreply.github.com>
2026-04-15 16:54:51 -0700

    Updated/Added simply-static-1-1776295980

--- wp-content/plugins/elementor-pro/core/app/modules/import-export-customization/assets/js/hooks/use-tier.js
+++ wp-content/plugins/elementor-pro/core/app/modules/import-export-customization/assets/js/hooks/use-tier.js
@@ -1,8 +1,1 @@
-export const isHighTier = () => {
-	try {
-		return 'expert' === elementorCommon?.config?.library_connect?.plan_type ||
-			'agency' === elementorCommon?.config?.library_connect?.plan_type;
-	} catch ( error ) {
-		return false;
-	}
-};
+export const isHighTier=()=>{try{return'expert'===elementorCommon?.config?.library_connect?.plan_type||'agency'===elementorCommon?.config?.library_connect?.plan_type;}catch(error){return false;}};
--- wp-content/plugins/elementor-pro/core/app/modules/site-editor/assets/js/data/component.js
+++ wp-content/plugins/elementor-pro/core/app/modules/site-editor/assets/js/data/component.js
@@ -1,13 +1,2 @@
-import * as dataCommands from './commands';
-
-export default class Component extends $e.modules.ComponentBase {
-	static namespace = 'site-editor';
-
-	getNamespace() {
-		return this.constructor.namespace;
-	}
-
-	defaultData() {
-		return this.importCommands( dataCommands );
-	}
-}
+import*as dataCommands from'./commands';export default class Component extends $e.modules.ComponentBase{static namespace='site-editor';getNamespace(){return this.constructor.namespace;}
+defaultData(){return this.importCommands(dataCommands);}}
