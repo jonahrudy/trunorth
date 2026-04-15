65f70b70d28994b124e0e93f95d939ac8cbb65b9
jonahrudy <110498996+jonahrudy@users.noreply.github.com>
2026-04-15 16:57:57 -0700

    Updated/Added simply-static-1-1776295980

--- wp-content/plugins/elementor/core/editor/loader/v2/js/editor-environment-v2.js
+++ wp-content/plugins/elementor/core/editor/loader/v2/js/editor-environment-v2.js
@@ -1,5 +1,2 @@
-if ( ! window.elementorV2?.env ) {
-	throw new Error( 'The "@elementor/env" package was not loaded.' );
-}
-
-window.elementorV2.env.initEnv( window.elementorEditorV2Env );
+if(!window.elementorV2?.env){throw new Error('The "@elementor/env" package was not loaded.');}
+window.elementorV2.env.initEnv(window.elementorEditorV2Env);
