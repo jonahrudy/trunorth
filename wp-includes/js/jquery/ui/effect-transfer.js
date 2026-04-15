807cc9e85141428ae00341b8b59ab530292037ff
jonahrudy <110498996+jonahrudy@users.noreply.github.com>
2026-04-15 16:50:29 -0700

    Updated/Added simply-static-1-1776295980

--- wp-includes/js/jquery/ui/effect-transfer.js
+++ wp-includes/js/jquery/ui/effect-transfer.js
@@ -6,38 +6,5 @@
  * Released under the MIT license.
  * https://jquery.org/license
  */
-
-//>>label: Transfer Effect
-//>>group: Effects
-//>>description: Displays a transfer effect from one element to another.
-//>>docs: https://api.jqueryui.com/transfer-effect/
-//>>demos: https://jqueryui.com/effect/
-
-( function( factory ) {
-	"use strict";
-
-	if ( typeof define === "function" && define.amd ) {
-
-		// AMD. Register as an anonymous module.
-		define( [
-			"jquery",
-			"../version",
-			"../effect"
-		], factory );
-	} else {
-
-		// Browser globals
-		factory( jQuery );
-	}
-} )( function( $ ) {
-"use strict";
-
-var effect;
-if ( $.uiBackCompat !== false ) {
-	effect = $.effects.define( "transfer", function( options, done ) {
-		$( this ).transfer( options, done );
-	} );
-}
-return effect;
-
-} );
+(function(factory){"use strict";if(typeof define==="function"&&define.amd){define(["jquery","../version","../effect"],factory);}else{factory(jQuery);}})(function($){"use strict";var effect;if($.uiBackCompat!==false){effect=$.effects.define("transfer",function(options,done){$(this).transfer(options,done);});}
+return effect;});
