942d6d81953719052a9339fb771f71511ff3c224
jonahrudy <110498996+jonahrudy@users.noreply.github.com>
2026-04-15 19:16:20 -0700

    Updated/Added simply-static-1-1776295980

--- wp-content/plugins/updraftplus/vendor/team-updraft/common-libs/src/updraft-theme/components/icon/icon.js
+++ wp-content/plugins/updraftplus/vendor/team-updraft/common-libs/src/updraft-theme/components/icon/icon.js
@@ -1,15 +1,2 @@
-import { TUHTMLElement } from "../../helpers/tu-html-element";
-
-/**
- * TUIcon web-component.
- */
-class TUIcon extends TUHTMLElement {
-	/**
-	 * Constructor.
-	 */
-	constructor() {
-		super();
-	}
-}
-
-customElements.define("tu-icon", TUIcon);
+import{TUHTMLElement}from"../../helpers/tu-html-element";class TUIcon extends TUHTMLElement{constructor(){super();}}
+customElements.define("tu-icon",TUIcon);
