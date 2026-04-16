8c02be5c29a362b725a3b8cdf7299f48374a69d7
jonahrudy <110498996+jonahrudy@users.noreply.github.com>
2026-04-15 17:17:17 -0700

    Updated/Added simply-static-1-1776295980

--- wp-content/themes/elementra/skins/default/skin-admin.js
+++ wp-content/themes/elementra/skins/default/skin-admin.js
@@ -1,11 +1,1 @@
-/* global jQuery, ELEMENTRA_STORAGE */
-
-( function() {
-	"use strict";
-
-	// Disable a "Title, Description, Link" parameters in out shortcodes
-	elementra_add_filter( 'trx_addons_filter_add_title_param', function( allow, sc ) {
-		return false;
-	} );
-
-} )();
+(function(){"use strict";elementra_add_filter('trx_addons_filter_add_title_param',function(allow,sc){return false;});})();
