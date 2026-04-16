aca4f64e0dae5d62b5a8180ebf1e0cdbae98f46a
jonahrudy <110498996+jonahrudy@users.noreply.github.com>
2026-04-15 17:16:10 -0700

    Updated/Added simply-static-1-1776295980

--- wp-content/themes/elementra/plugins/elementor/params/choice/choice-control.js
+++ wp-content/themes/elementra/plugins/elementor/params/choice/choice-control.js
@@ -1,13 +1,1 @@
-/*
-global: elementor, jQuery;
-*/
-jQuery(document).ready(function(){
-	"use strict";
-	// Generate Elementor-specific event when field is changed:
-	// input	for @ui.input, @ui.textarea
-	// change	for @ui.checkbox, @ui.radio, @ui.select
-	// click	for @ui.responsiveSwitchers
-	jQuery('#elementor-panel').on('change', '.elementra_param_choice input', function(e) {
-		jQuery(this).trigger('input');
-	});
-});
+jQuery(document).ready(function(){"use strict";jQuery('#elementor-panel').on('change','.elementra_param_choice input',function(e){jQuery(this).trigger('input');});});
