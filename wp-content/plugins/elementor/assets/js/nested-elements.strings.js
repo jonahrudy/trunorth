f873e13d474ef7d51c8430a6bdfa144b8f527b30
jonahrudy <110498996+jonahrudy@users.noreply.github.com>
2026-04-17 10:42:14 -0700

    Updated/Added simply-static-1-1776447360

--- wp-content/plugins/elementor/assets/js/nested-elements.strings.js
+++ wp-content/plugins/elementor/assets/js/nested-elements.strings.js
@@ -1,1 +1,4 @@
-__('Close','elementor');__('Select your Structure','elementor');__('Add new container','elementor');__('Drag widget here','elementor');
+__( 'Close', 'elementor' );
+__( 'Select your Structure', 'elementor' );
+__( 'Add new container', 'elementor' );
+__( 'Drag widget here', 'elementor' );
