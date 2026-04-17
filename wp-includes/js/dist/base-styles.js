f9fc48fb722705000242e3a14225e28efcfaaa80
jonahrudy <110498996+jonahrudy@users.noreply.github.com>
2026-04-17 12:25:21 -0700

    Updated/Added simply-static-1-1776447360

--- wp-includes/js/dist/base-styles.js
+++ wp-includes/js/dist/base-styles.js
@@ -1,1 +1,10 @@
-(()=>{var __webpack_exports__={};(window.wp=window.wp||{}).baseStyles=__webpack_exports__;})();
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/**
+ * This file is intentionally left blank and acts as
+ * a resolvable JavaScript entry point for this package.
+ */
+
+(window.wp = window.wp || {}).baseStyles = __webpack_exports__;
+/******/ })()
+;
