3fb0e2b0671fae1085ba649ba766ac890e181661
jonahrudy <110498996+jonahrudy@users.noreply.github.com>
2026-04-17 11:07:20 -0700

    Updated/Added simply-static-1-1776447360

--- wp-content/themes/elementra/plugins/sitepress-multilingual-cms/sitepress-multilingual-cms-options.js
+++ wp-content/themes/elementra/plugins/sitepress-multilingual-cms/sitepress-multilingual-cms-options.js
@@ -1,1 +1,15 @@
-jQuery(document).ready(function(){"use strict";var fld=jQuery('input[name*="wpml_current_language"]');if(fld.length>0){fld.val(fld.val()+'!');}});
+/*
+ * Touch the field 'wpml_current_language' to enable store it to the theme_mods in the Theme Options
+ */
+
+/* global jQuery */
+
+jQuery( document ).ready(
+	function() {
+		"use strict";
+		var fld = jQuery( 'input[name*="wpml_current_language"]' );
+		if (fld.length > 0) {
+			fld.val( fld.val() + '!' );
+		}
+	}
+);
