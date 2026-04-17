ffae5c70866808a0bf8b0092c9a7dc94413638d6
jonahrudy <110498996+jonahrudy@users.noreply.github.com>
2026-04-17 11:08:44 -0700

    Updated/Added simply-static-1-1776447360

--- wp-content/themes/elementra/js/colorpicker/spectrum/i18n/jquery.spectrum-fa.js
+++ wp-content/themes/elementra/js/colorpicker/spectrum/i18n/jquery.spectrum-fa.js
@@ -1,1 +1,14 @@
-(function($){var localization=$.spectrum.localization["fa"]={cancelText:"لغو",chooseText:"انتخاب"};$.extend($.fn.spectrum.defaults,localization);})(jQuery);
+// Spectrum Colorpicker
+// Persian (fa) localization
+// https://github.com/bgrins/spectrum
+
+(function ( $ ) {
+
+    var localization = $.spectrum.localization["fa"] = {
+        cancelText: "لغو",
+        chooseText: "انتخاب"
+    };
+
+    $.extend($.fn.spectrum.defaults, localization);
+
+})( jQuery );
--- wp-content/themes/elementra/js/colorpicker/spectrum/i18n/jquery.spectrum-fi.js
+++ wp-content/themes/elementra/js/colorpicker/spectrum/i18n/jquery.spectrum-fi.js
@@ -1,1 +1,14 @@
-(function($){var localization=$.spectrum.localization["fi"]={cancelText:"Kumoa",chooseText:"Valitse"};$.extend($.fn.spectrum.defaults,localization);})(jQuery);
+// Spectrum Colorpicker
+// Finnish (fi) localization
+// https://github.com/bgrins/spectrum
+
+(function ( $ ) {
+
+    var localization = $.spectrum.localization["fi"] = {
+        cancelText: "Kumoa",
+        chooseText: "Valitse"
+    };
+
+    $.extend($.fn.spectrum.defaults, localization);
+
+})( jQuery );
