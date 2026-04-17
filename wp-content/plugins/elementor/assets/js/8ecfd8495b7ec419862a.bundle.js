cbd22cc80f16dcf6cf8e623cb615658b9f439f61
jonahrudy <110498996+jonahrudy@users.noreply.github.com>
2026-04-17 10:42:53 -0700

    Updated/Added simply-static-1-1776447360

--- wp-content/plugins/elementor/assets/js/8ecfd8495b7ec419862a.bundle.js
+++ wp-content/plugins/elementor/assets/js/8ecfd8495b7ec419862a.bundle.js
@@ -1,5 +1,14 @@
-"use strict";(self["webpackChunkelementor"]=self["webpackChunkelementor"]||[]).push([["node_modules_elementor_elementor-one-assets_locales_tr-TR_assets-whatsnew_json"],{"../node_modules/@elementor/elementor-one-assets/locales/tr-TR/assets-whatsnew.json":
+"use strict";
+(self["webpackChunkelementor"] = self["webpackChunkelementor"] || []).push([["node_modules_elementor_elementor-one-assets_locales_tr-TR_assets-whatsnew_json"],{
+
+/***/ "../node_modules/@elementor/elementor-one-assets/locales/tr-TR/assets-whatsnew.json":
 /*!******************************************************************************************!*\
   !*** ../node_modules/@elementor/elementor-one-assets/locales/tr-TR/assets-whatsnew.json ***!
   \******************************************************************************************/
-((module)=>{module.exports={};})}]);
+/***/ ((module) => {
+
+module.exports = {};
+
+/***/ })
+
+}]);
